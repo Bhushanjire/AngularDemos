b2da6b1192b4af8c55204d2b2ece4a2f26b635c2
bhushan <bhushanjire@gmail.com>
2019-12-02 16:40:09 +0530

    "custom module"

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,35 @@
 const express = require('express')
+const airthmetic = require('./src/app/custom-module/airthmetic-opp');
+const bodyParser = require("body-parser");
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({
+  extended: true,
+  
+}));
+
+app.use(bodyParser.json());
+//default router
+app.get('/', function (req : any, res : any) {
+  res.send('Hello World');
+});
+
+app.post('/add',function(req : any,res:any){
+  let value1 = req.body.value1 ? req.body.value1 : 0;
+  let value2 = req.body.value2 ? req.body.value2:0
+  let result={
+    "addition" : airthmetic.addition(value1,value2)
+  }
+  res.send(result)
+});
+
+app.post('/substract',function(req : any,res:any){
+  let value1 = req.body.value1 ? req.body.value1 : 0;
+  let value2 = req.body.value2 ? req.body.value2:0
+  let result={
+    "substract" : airthmetic.substract(value1,value2)
+  }
+  res.send(result)
 });
 
 app.listen(3000, function () {
---
+++ server/src/app/custom-module/airthmetic-opp.ts
@@ -0,0 +1,7 @@
+exports.addition =function (a: number, b: number) {
+    return a + b;
+}
+exports.substract =function (a: number, b: number) {
+    return a - b;
+}
+
